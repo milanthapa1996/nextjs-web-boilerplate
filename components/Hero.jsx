dac95d77b9de19d4419b470d466b68e1c35c22f0
milanthapa1996 <tmilan0604@gmail.com>
2022-09-14 19:08:51 +0545

    bug fixing

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,26 +9,27 @@ const Hero = () => {
           <Link
             href="/courses"
             className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
-            role="alert"
           >
-            <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">
-              New
-            </span>{" "}
-            <span className="text-sm font-medium">
-              Admission 2022 open for all courses
-            </span>
-            <svg
-              className="ml-2 w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
+            <>
+              <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">
+                New
+              </span>{" "}
+              <span className="text-sm font-medium">
+                Admission 2022 open for all courses
+              </span>
+              <svg
+                className="ml-2 w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </>
           </Link>
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
             NEPAL&apos;S NO. 1 CADET PREPARATION CLASSES
@@ -54,7 +55,7 @@ const Hero = () => {
                 <path
                   fill-rule="evenodd"
                   d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
             </a>
